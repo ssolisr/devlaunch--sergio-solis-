297962c8579fc5100088805e5e39d0ea7843fc44
Sergio Solis Romagosa <sergy920@gmail.com>
2025-02-17 07:14:15 -0600

    feat(polymorphism): add instrument exercise

---
+++ object-oriented-programming/polymorphism/instrument.ts
@@ -0,0 +1,61 @@
+type InstrumentType = 'viento' | 'cuerda' | 'percusión'
+
+abstract class Instrument {
+
+  constructor(public type: InstrumentType) {
+
+  }
+
+  play() {} 
+}
+
+class Guitar extends Instrument {
+  constructor() {
+    super('cuerda')
+  }
+
+  play(): void {
+    console. log('🎸 Strum strum strum...')
+  }
+}
+
+class Flute extends Instrument {
+  constructor() {
+    super('viento')
+  }
+
+  play(): void {
+    console. log('🪈 Toot toot toot...')
+  }
+}
+
+class Drums extends Instrument {
+  constructor() {
+    super('percusión')
+  }
+
+  play(): void {
+    console. log('🥁 Boom boom boom')
+  }
+}
+
+
+class Artist {
+  constructor() {}
+  
+  playInstrument (instrument: Instrument) {
+    instrument. play()
+    console.log(instrument.type)
+  }
+}
+
+
+const guitar1 = new Guitar()
+const flute1 = new Flute()
+const drums1 = new Drums()
+
+const artist1 = new Artist()
+
+artist1.playInstrument(guitar1)
+artist1.playInstrument(flute1)
+artist1.playInstrument(drums1)
